32dacf40deca92739cc86647262fd7209ee47f92
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-08-27 11:44:03 +1000

    Remove unnecessary code

--- AlertViewBotDemo/ViewController.swift
+++ AlertViewBotDemo/ViewController.swift
@@ -1,25 +1,11 @@
-//
-//  ViewController.swift
-//  AlertViewBotDemo
-//
-//  Created by Evgenii on 27/08/2015.
-//  Copyright © 2015 Evgenii Neumerzhitckii. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
   @IBAction func didTapOpenAlertButton(sender: AnyObject) {
     openAlertView()
   }
@@ -38,7 +24,7 @@ class ViewController: UIViewController {
     if let textField = alertView.textFieldAtIndex(0) {
       textField.placeholder = title
 //      textField.autocapitalizationType = UITextAutocapitalizationType.Sentences
-      textField.autocorrectionType = UITextAutocorrectionType.Default
+//      textField.autocorrectionType = UITextAutocorrectionType.Default
     }
     
     alertView.show()
