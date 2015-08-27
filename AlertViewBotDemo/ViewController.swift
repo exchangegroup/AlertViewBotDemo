f7943961cc1995cf5919c31304fd1d914cc6bcd1
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-08-27 11:11:33 +1000

    Add readme

--- AlertViewBotDemo/ViewController.swift
+++ AlertViewBotDemo/ViewController.swift
@@ -19,6 +19,32 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func didTapOpenAlertButton(sender: AnyObject) {
+    openAlertView()
+  }
+  
+  private func openAlertView() {
+    let title = "Email"
+    
+    let alertView = UIAlertView(title: title,
+      message: "",
+      delegate: nil, cancelButtonTitle: nil,
+      otherButtonTitles: "OK", "Cancel")
+    
+    alertView.cancelButtonIndex = 1
+    alertView.alertViewStyle = UIAlertViewStyle.PlainTextInput
+    
+    if let textField = alertView.textFieldAtIndex(0) {
+      textField.placeholder = title
+      textField.autocapitalizationType = UITextAutocapitalizationType.Sentences
+      textField.autocorrectionType = UITextAutocorrectionType.Default
+      textField.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+      textField.clearButtonMode = UITextFieldViewMode.Always
+    }
+    
+    alertView.show()
+  }
 
 
 }
---
+++ README.md
@@ -0,0 +1,14 @@
+# iOS demo app: UI Test fails in Xcode bot for alert view with text box
+
+An iOS app demo that reproduces an issue with UI test. In the UI test we open a UIAlertView and fill the text box.
+
+UI test runs successfully in Xcode but fails when run by Xcode bot:
+
+> Assertion Failure: UI Testing Failure - failed: Timed out waiting for key event to complete
+
+
+## Software used
+
+Server API v6
+SDKs: OS X 15A244e, watchOS 13S5325c, iOS 13A4325c
+OS X 10.11 (15A263e), Xcode 7.0 (7A192o), OS X Server 5.0.1 (15S2235)
